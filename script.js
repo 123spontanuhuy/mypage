7d4076073c3e792eb09fd14f6962aa32119144bc
123spontanuhuy <99772606+123spontanuhuy@users.noreply.github.com>
2023-11-14 23:17:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ navMenu.classList.toggle('hide');
 
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
 
     // Get form data
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Simulate sending email (console log for demonstration purposes)
+    
     console.log('Name:', name);
     console.log('Email:', email);
     console.log('Message:', message);
 
-    // Optionally, you can display a success message to the user
+  
     alert('Message sent! (This is a simulation, no email has been sent)');
     
-    // Clear the form fields
+   
     this.reset();
 });
